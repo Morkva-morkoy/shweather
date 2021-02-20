9c145c67317cd8ad4dd4f23a8659b62775602763
Stepan <66877869+morkva-morkoy@users.noreply.github.com>
2021-02-20 18:38:22 +0300

    d

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('a.html', temp=temp)
+@app.route('/sing-in')
+def log():
+    return render_template('b.html', temp=temp)
 
 
 if __name__ == '__main__':
