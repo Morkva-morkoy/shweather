7ee168b91c8d33253658dfff728a33c227573521
SHLYAPA <stepa2429999@gmail.com>
2021-02-21 22:11:37 +0300

    add normal d

--- app.py
+++ app.py
@@ -6,17 +6,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('a.html', temp=temp)
+    return render_template('d.html')
 
 
 @app.route('/sing-in')
 def log():
-    return render_template('b.html', temp=temp)
+    return render_template('b.html')
 
 
 @app.route('/about-us')
 def about():
-    return render_template('c.html', temp=temp)
+    return render_template('c.html')
+
+
+@app.route('/shweather')
+def shweather():
+    return render_template('a.html', temp=temp)
 
 
 if __name__ == '__main__':
