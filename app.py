cf532e52d5cc8aaa034da2c4fe2a3912ac67d69e
SHLYAPA <stepa2429999@gmail.com>
2021-03-20 22:44:49 +0300

    removed SQLAlchemy

--- app.py
+++ app.py
@@ -5,20 +5,9 @@ from flask import request
 from db import cursor
 import sqlite3
 import random
-from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////db1.db'
-db = SQLAlchemy(app)
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
 
 
 @app.route("/get_my_ip", methods=["GET"])
